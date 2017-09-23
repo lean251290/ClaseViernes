61a4246a7d62f2998078b04150311e87937a2051
lean251290 <lean251290@gmail.com>
2017-09-22 22:53:59 -0300

    practico 2

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import Vista.calculadoraVista;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        calculadoraVista calc1 = new calculadoraVista();
+        calc1.setVisible(true);
+        }
+    
+    
+}
